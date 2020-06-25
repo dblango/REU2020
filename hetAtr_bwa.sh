b909d0210778b0f8b9a9abbdfa9a46b25fda6b16
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-25 15:52:13 -0400

    Update hetAtr_bwa.sh

--- hetAtr_bwa.sh
+++ hetAtr_bwa.sh
@@ -14,15 +14,6 @@ module load bwa/0.7.17-fasrc01
 bwa index -p hetAtr 00_genome/GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
 
 
-for R1 in *1.fastq.gz;
-do
-sbatch map_samples.sh $R1 $R2
-wait 1 sec
-done
 
 
-R1=$1
-name=`echo $R1 | sed ‘s/_1.fastq.gz\+//‘`
-R2=${name}_2.fastq.gz
-#run mapping
-bwa mem -t 8 -R ‘@RG\tID:‘$name’\tSM:‘$name’ hetAtr $R1 $R2 > $name.sam
+
