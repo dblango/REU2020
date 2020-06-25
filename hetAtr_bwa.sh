09482da55a72217103de28e7a1773548bb2e2a20
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-25 14:37:06 -0400

    bwa script to run on real hetAtr data

--- hetAtr_bwa.sh
+++ hetAtr_bwa.sh
@@ -12,6 +12,10 @@
 
 module load bwa/0.7.17-fasrc01
 
-mkdir -p hetAtr/00_genome
 bwa index -p hetAtr 00_genome/GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
-bwa mem -t 8 hetAtr hetAtr_ind01_L1_573_1.fastq.gz hetAtr_ind01_L1_573_2.fastq.gz > hetAtr_ind01_L1_573.sam
+for R1 in *1.fastq.gz;
+do
+name=`echo $R1 | sed ‘s/_1.fastq.gz\+//‘`
+R2=${name}_2.fastq.gz
+bwa mem -t 8 -R ‘@RG\tID:‘$name’\tSM:‘$name’ hetAtr $R1 $R2 > $name.sam
+done
