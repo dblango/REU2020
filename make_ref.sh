e02d97fe4177f62e561b5f5549cf1a6e5243ca26
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-25 15:57:33 -0400

    Update make_ref.sh

--- make_ref.sh
+++ make_ref.sh
@@ -7,7 +7,7 @@
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
-# submit this script from your /n/holyscratch01/informatics/hetAtr by typing sbatch hetAtr_bwa.sh
+# submit this script from your /n/holyscratch01/informatics/hetAtr by typing sbatch make_ref.sh
 
 
 module load bwa/0.7.17-fasrc01
