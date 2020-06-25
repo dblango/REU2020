b7604dc960b6dec683c166df8cb34544f1129ab3
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-25 13:31:52 -0400

    running bwa on one pair of species to check

--- hetAtr_bwa.sh
+++ hetAtr_bwa.sh
@@ -2,11 +2,15 @@
 #SBATCH -p shared
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem 1000
+#SBATCH --mem 8000 
 #SBATCH -t 30
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
-# submit this script from your home directory by typing sbatch helloworld.sh
+# submit this script from your home directory by typing sbatch hetAtr_bwa.sh
 
-touch hello_world.txt
+ls -l $HOME > homedir_contents.txt
+srun --pty -p test -t 40 --mem 8000 /bin/bash
+module load bwa/0.7.17-fasrc01
+bwa index -p taeGut GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
+bwa mem -t 1 taeGut hetAtr_ind01_L1_573_1.fastq.gz hetAtr_ind01_L1_573_2.fastq.gz > hetAtr_ind01_L1_573.sam
