d3c91f8c59bbcc14782036adaedfc6b79593e33d
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-25 14:04:29 -0400

    Update hetAtr_bwa.sh

--- hetAtr_bwa.sh
+++ hetAtr_bwa.sh
@@ -7,9 +7,11 @@
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
-# submit this script from your home directory by typing sbatch hetAtr_bwa.sh
+# submit this script from your /n/holyscratch01/informatics/hetAtr by typing sbatch hetAtr_bwa.sh
 
 
 module load bwa/0.7.17-fasrc01
-bwa index -p hetAtr GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
+
+mkdir -p hetAtr/00_genome
+bwa index -p hetAtr 00_genome/GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
 bwa mem -t 8 hetAtr hetAtr_ind01_L1_573_1.fastq.gz hetAtr_ind01_L1_573_2.fastq.gz > hetAtr_ind01_L1_573.sam
