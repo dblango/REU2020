beff8173f71a68c4d72827ece8213b7d96f086b8
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-26 12:01:09 -0400

    Update map_samples.sh

--- map_samples.sh
+++ map_samples.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 8000
+#SBATCH -t 120
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+
+# submit this script from your /n/holyscratch01/informatics/hetAtr by typing sbatch map_samples.sh
+
 module load bwa/0.7.17-fasrc01
 
 R1=$1
 name=`echo $R1 | sed ‘s/_1.fastq.gz\+//‘`
 R2=${name}_2.fastq.gz
 #run mapping
-bwa mem -t 8 -R ‘@RG\tID:‘$name’\tSM:‘$name’ hetAtr $R1 $R2 > $name.sam
+bwa mem -t 8 -R ‘@RG\tID:‘$name’\tSM:‘$name’ 00_genome/hetAtr $R1 $R2 > $name.sam
