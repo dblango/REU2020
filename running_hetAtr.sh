0370e19d2118e46674275d9fef37594a9a441ef5
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-26 13:25:45 -0400

    Update running_hetAtr.sh

--- running_hetAtr.sh
+++ running_hetAtr.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-#SBATCH -p shared
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --mem 8000
-#SBATCH -t 60
-#SBATCH -o slurm.%N.%j.out
-#SBATCH -e slurm.%N.%j.err
-
-# submit this script from your /n/holyscratch01/informatics/hetAtr by typing sbatch running_hetAtr.sh
-
 for R1 in *1.fastq.gz;
 do
 sbatch map_samples.sh $R1 $R2
 sleep 1
 done
+
+# run this by doing ./running_hetAtr in the /n/holyscratch01/informatics/blangod/hetAtr
