13d50aca5d16ab38501831c5999a78a184413136
DymonB <67061855+dblango@users.noreply.github.com>
2020-06-27 23:21:39 -0400

    Update running_hetAtr.sh

--- running_hetAtr.sh
+++ running_hetAtr.sh
@@ -4,4 +4,4 @@ sbatch map_samples.sh $R1 $R2
 sleep 1
 done
 
-# run this by doing ./running_hetAtr in the /n/holyscratch01/informatics/blangod/hetAtr
+# run this by doing ./running_hetAtr.sh in the /n/holyscratch01/informatics/blangod/hetAtr
