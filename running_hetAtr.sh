a0e1e3c3e405545880108d2145503c94181e398b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-30 12:34:57 -0400

    Update running_hetAtr.sh

--- running_hetAtr.sh
+++ running_hetAtr.sh
@@ -1,6 +1,6 @@
 # run this by typing ./running_hetAtr.sh in /n/holyscratch01/informatics/blangod/hetAtr
 
-for R1 in *1.fastq.gz;
+for R1 in 01_fastqs/*1.fastq.gz;
 do
 sbatch map_samples.sh $R1 $R2
 sleep 1
