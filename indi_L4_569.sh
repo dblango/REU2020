d78237667dffc4135fea2810eef1e1115f4ac6f2
DymonB <67061855+dblango@users.noreply.github.com>
2020-07-02 09:57:21 -0400

    Create indi_L4_569.sh

---
+++ indi_L4_569.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 8000
+#SBATCH -t 0-08:00:00
+#SBATCH -o bwa_mem.%N.%j.out
+#SBATCH -e bwa_mem.%N.%j.err
+
+# submit this script from /n/holyscratch01/informatics/blangod/hetAtr/ by typing sbatch map_samples.sh
+
+module load bwa/0.7.17-fasrc01
+
+R1=$hetAtr_ind12_L4_569_1.fastq.gz
+name=`echo $R1 | sed 's/_1.fastq.gz\+//'`
+R2=${name}_2.fastq.gz
+#run mapping
+bwa mem -t 8 -R '@RG\tID:${name}\tSM:${name}' 00_genome/hetAtr $R1 $R2 > $name.sam
