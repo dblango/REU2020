ce6cdc4cefc3c9d8be6ea87790f4a0e373f71a91
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-03 04:27:42 -0400

    Update het_bams_sjsw.sh

--- het_bams_sjsw.sh
+++ het_bams_sjsw.sh
@@ -14,6 +14,6 @@ name=`echo $SP | sed 's/.sam\+//'`
 samtools view -b -h -o $name.bam $name.sam
 samtools sort -o $name.sort.bam $name.bam
 samtools index $name.sort.bam
-samtools fixmate -r -m -O bam $name.sort.bam $name.fm.bam
-samtools markdup -r -s -f $name.stats.out $name.fm.bam $name.clean.bam
+#samtools fixmate -r -m -O bam $name.sort.bam $name.fm.bam
+#samtools markdup -r -s -f $name.stats.out $name.fm.bam $name.clean.bam
 samtools quickcheck $name.final.bam
