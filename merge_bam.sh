6b7dbde088a26f47fd6d1b0d267dcf3881fe03d6
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-06 12:47:02 -0400

    Update merge_bam.sh

--- merge_bam.sh
+++ merge_bam.sh
@@ -4,8 +4,8 @@
 #SBATCH -n 1
 #SBATCH --mem 8000
 #SBATCH -t 0-08:00:00
-#SBATCH -o het_bams.%N.%j.out
-#SBATCH -e het_bams.%N.%j.err
+#SBATCH -o bam_merge.%N.%j.out
+#SBATCH -e bam_merge.%N.%j.err
 
 module load samtools/1.10-fasrc01
 
