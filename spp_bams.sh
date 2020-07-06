236ba29d22e278f7aa4d0e17bee7a0039923d51a
DymonB <67061855+dblango@users.noreply.github.com>
2020-07-06 11:42:54 -0400

    Update spp_bams.sh

--- spp_bams.sh
+++ spp_bams.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 8000
+#SBATCH -t 0-08:00:00
+#SBATCH -o het_bams.%N.%j.out
+#SBATCH -e het_bams.%N.%j.err
+
 module load samtools/1.10-fasrc01 SP=$1
+
 name=`echo $SP | sed 's/.sam\+//'`
 samtools view -b -h -o $name.bam $name.sam
 samtools sort -o $name.sort.bam $name.bam
