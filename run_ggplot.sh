77772268acaef4a052f593397af5249198c79b60
DymonB <67061855+dblango@users.noreply.github.com>
2020-07-18 11:04:07 -0400

    Create run_ggplot.sh

---
+++ run_ggplot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -t 0-48:00
+#SBATCH -p shared
+#SBATCH --mem=9000
+#SBATCH -o ggplots_%j.out
+#SBATCH -e ggplots_%j.err
+
+# submit from /n/holyscratch01/informatics/blangod/hetAtr/02_bams
+
+module load R/4.0.2-fasrc01
+export R_LIBS_USER=$HOME/apps/R_4.0.2:$R_LIBS_USER
+
+Rscript ggplot_script.R
