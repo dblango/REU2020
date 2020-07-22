89bba3e203baf5f1efa84d650eb4bd2e15a636d5
DymonB <67061855+dblango@users.noreply.github.com>
2020-07-22 15:41:12 -0400

    Update fem_bam.sh

--- fem_bam.sh
+++ fem_bam.sh
@@ -9,11 +9,5 @@
 
 module load samtools/1.10-fasrc01
 
-samtools merge -f -b indv4.txt indv4.final.bam fem.final.bam
-samtools merge -f -b indv5.txt indv5.final.bam fem.final.bam
-samtools merge -f -b indv6.txt indv6.final.bam fem.final.bam
-samtools merge -f -b indv7.txt indv7.final.bam fem.final.bam
-samtools merge -f -b indv8.txt indv8.final.bam fem.final.bam
-samtools merge -f -b indv9.txt indv9.final.bam fem.final.bam 
-samtools merge -f -b indv10.txt indv10.final.bam fem.final.bam
-samtools merge -f -b indv14.txt indv14.final.bam fem.final.bam
+samtools merge fem.final.bam indv4.final.bam indv5.final.bam indv6.final.bam indv7.final.bam indv8.final.bam indv9.final.bam indv10.final.bam indv14.final.bam
+
