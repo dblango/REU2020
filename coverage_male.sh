bae4a194e4c55b99a861713be73a55890ea56517
DymonB <67061855+dblango@users.noreply.github.com>
2020-07-22 16:16:49 -0400

    Create coverage_male.sh

---
+++ coverage_male.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 8000
+#SBATCH -t 0-12:00:00
+#SBATCH -o bam_merge.%N.%j.out
+#SBATCH -e bam_merge.%N.%j.err
+
+module load samtools/1.10-fasrc01
+
+samtools coverage male.final.bam > male_bam.out
